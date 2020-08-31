9c53db0dec30a01e3e2c1071754236091b563ed7
Devon Gardner <dgardner365@gmail.com>
2020-08-31 13:03:07 -0400

    Added filepath variables to file_parsing prep

--- Week 2/prep/file_parsing.py
+++ Week 2/prep/file_parsing.py
@@ -2,13 +2,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import pandas as pd
+import os
 
-iris_panda = pd.read_table('iris.data', delimiter=',')
-iris = np.genfromtxt('iris.data', delimiter=',', dtype='unicode')
+current_directory = os.path.dirname(__file__)
+filepath = os.path.join(current_directory, "..", "data", "iris.data")
+
+iris_panda = pd.read_table( filepath, delimiter=',' )
+iris = np.genfromtxt( filepath, delimiter=',', dtype='unicode' )
 iris_frame = pd.DataFrame(iris, columns=iris[0])
 iris_frame = iris_frame[1:]
 
 sns.set(style="ticks", color_codes=True)
 frame = sns.pairplot(iris_frame, hue="class")
 panda = sns.pairplot(iris_panda, hue="class")
-plt.show()
+#plt.show()
+
+print(iris_frame)
+print(iris_panda)
