a65bd5fa84a2cfa79f265c2ba72c698d1b19dc10
Devon Gardner <dgardner365@gmail.com>
2020-08-31 14:01:54 -0400

    added statistics calculation and displayment to file_parsing prep

--- Week 2/prep/file_parsing.py
+++ Week 2/prep/file_parsing.py
@@ -4,18 +4,37 @@ import seaborn as sns
 import pandas as pd
 import os
 
-current_directory = os.path.dirname(__file__)
-filepath = os.path.join(current_directory, "..", "data", "iris.data")
+def main():
+    '''Imports Iris dataset and visualizes it.'''
 
-iris_panda = pd.read_table( filepath, delimiter=',' )
-iris = np.genfromtxt( filepath, delimiter=',', dtype='unicode' )
-iris_frame = pd.DataFrame(iris, columns=iris[0])
-iris_frame = iris_frame[1:]
+    # Get current directory and set path to data file.
+    current_directory = os.path.dirname(__file__)
+    filepath = os.path.join(current_directory, "..", "data", "iris.data")
 
-sns.set(style="ticks", color_codes=True)
-frame = sns.pairplot(iris_frame, hue="class")
-panda = sns.pairplot(iris_panda, hue="class")
-#plt.show()
+    # Import data as a pandas dataframe.
+    iris_panda = pd.read_table( filepath, delimiter=',' )
 
-print(iris_frame)
-print(iris_panda)
+    # Generate ndarray from data and convert it to pandas dataframe.
+    iris = np.genfromtxt( filepath, delimiter=',', dtype='unicode' )
+    iris_frame = pd.DataFrame(iris, columns=iris[0])
+    iris_frame = iris_frame[1:]
+
+    # Style, plot, and show data.
+    sns.set(style="ticks", color_codes=True)
+    frame = sns.pairplot(iris_frame, hue="Species")
+    panda = sns.pairplot(iris_panda, hue="Species")
+    plt.show()
+
+    # Calculate Stats
+    mean = np.mean( iris_panda )
+    std = np.std( iris_panda )
+    var = np.var( iris_panda )
+    # cov = np.cov( iris_panda )
+
+    # Display Stats
+    print(mean)
+    print(std)
+    print(var)
+    # print(cov)
+
+main()
