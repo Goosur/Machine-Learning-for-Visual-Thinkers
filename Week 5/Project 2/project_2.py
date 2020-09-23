5df73d5b2f53f4f28e4f5acc4726a564ce6af49e
Devon Gardner <dgardner365@gmail.com>
2020-09-22 22:50:21 -0400

    Added z score and range normalization functions to project 2.

--- Week 5/Project 2/project_2.py
+++ Week 5/Project 2/project_2.py
@@ -24,7 +24,7 @@ def read_from_file(file_name):
 
     return data
 
-def visualize(data, vars, x1, y1, x2, title, hue=None):
+def visualize(data, vars, title, hue=None):
     '''
     Plot the following types of visualizations: pair plot, joint plot, bar plot, and box plot.
 
@@ -43,13 +43,6 @@ def visualize(data, vars, x1, y1, x2, title, hue=None):
     sns.set(style="ticks", color_codes=True)
     pairplot = sns.pairplot(data=data, vars=vars, hue=hue)
     pairplot.fig.suptitle(title)
-    jointplot = sns.jointplot(data=data, x=x1, y=y1, hue=hue, kind="kde")
-    jointplot.fig.suptitle(title)
-    fig, ax = plt.subplots(ncols=2)
-    fig.set_size_inches(16,8)
-    plt.suptitle(title)
-    bar = sns.barplot(x=data[x2], y=data[y1], ax=ax[0])
-    box = sns.boxplot(x=data[x2], y=data[y1], ax=ax[1])
 
 def compute(data):
     '''
@@ -66,6 +59,73 @@ def compute(data):
     print("\nCovariance:")
     print( data.cov() )
 
+def norm_range(data):
+    '''
+    Normalize data by range.
+    \nINPUTS: data (matrix)
+    \nOUTPUTS: data normalized by range (matrix)
+    '''
+    # Prepare data for transformations by adding homogeneous 
+    ones = np.ones((data.shape[0], 1))
+    A = np.hstack((data, ones))
+
+    # Compute some stats
+    mins = np.min(A, axis=0).reshape((1, A.shape[1]))
+    maxs = np.max(A, axis=0).reshape((1 ,A.shape[1]))
+    ranges = (maxs - mins).reshape((1, A.shape[1]))
+
+    # Translation matrix
+    T = np.eye(A.shape[1])
+    T[0:-1,-1] = -mins[0,0:-1]
+
+    # Scale matrix
+    S = np.eye(A.shape[1])
+    S[0:-1,0:-1] = S[0:-1,0:-1] * (1/ranges[0,0:-1])
+
+    # Normalization matrix
+    N = S @ T
+
+    # Normalize the dataset.
+    A_norm = (N @ A.T).T
+
+    # Remove homogeneous column.
+    A_norm = np.delete(A_norm, -1, 1)
+
+    return A_norm
+
+def norm_z(data):
+    '''
+    Normalize data by standard deviation.
+    \nINPUTS: data (matrix)
+    \nOUTPUTS: data normalized by standard deviation (matrix)
+    '''
+    # Prepare data for transformations by adding homogeneous 
+    ones = np.ones((data.shape[0], 1))
+    A = np.hstack((data, ones))
+
+    # Compute some stats
+    means = np.mean(A, axis=0).reshape((1, A.shape[1]))
+    stds = np.std(A, axis=0).reshape((1, A.shape[1]))
+
+    # Translation matrix
+    T = np.eye(A.shape[1])
+    T[0:-1,-1] = -means[0,0:-1]
+
+    # Scale matrix
+    S = np.eye(A.shape[1])
+    S[0:-1,0:-1] = S[0:-1,0:-1] * (1/stds[0,0:-1])
+
+    # Normalization matrix
+    N = S @ T
+
+    # Normalize the dataset.
+    A_norm = (N @ A.T).T
+
+    # Remove homogeneous column.
+    A_norm = np.delete(A_norm, -1, 1)
+
+    return A_norm
+
 def main():
     iris_file = "iris.data"
     cars_file = "Cars2015.csv"
@@ -74,27 +134,45 @@ def main():
     iris = read_from_file(iris_file)
     cars = read_from_file(cars_file)
 
-    # Print datasets and headers.
-    print('\nIRIS HEADERS')
-    print(iris.columns)
-    print('\nIRIS DATA')
-    print(iris)
-    print('\nCARS HEADERS')
-    print(cars.columns)
-    print('\nCARS DATA')
-    print(cars)
-
     # Remove non numeric data.
     iris_numeric = iris.select_dtypes(exclude=object)
     cars_numeric = cars.select_dtypes(exclude=object)
 
-    # Compute stats on numeric data.
-    compute(iris_numeric)
-    compute(cars_numeric)
+    # Normalize numeric data by range and restore dataframe metadata.
+    iris_norm_range = pd.DataFrame(norm_range(iris_numeric.to_numpy()))
+    iris_norm_range.columns = iris_numeric.columns
+    cars_norm_range = pd.DataFrame(norm_range(cars_numeric.to_numpy()))
+    cars_norm_range.columns = cars_numeric.columns
 
-    # Visualize data.
-    visualize(data=iris, vars=iris_numeric.columns, x1=iris.columns[2], y1=iris.columns[3], x2=iris.columns[4], title='Iris', hue=iris.columns[-1])
-    visualize(data=cars, vars=cars_numeric.columns, x1=cars.columns[16], y1=cars.columns[6], x2=cars.columns[2], title='Cars', hue=cars.columns[2])
+    # Normalize numeric data by standard deviation and restore dataframe metadata.
+    iris_norm_z = pd.DataFrame(norm_z(iris_numeric.to_numpy()))
+    iris_norm_z.columns = iris_numeric.columns
+    cars_norm_z = pd.DataFrame(norm_z(cars_numeric.to_numpy()))
+    cars_norm_z.columns = cars_numeric.columns
+
+    # Compute stats on numeric data and normalized numeric data.
+    # print("Iris numeric stats:\n")
+    # compute(iris_numeric)
+    # print("\nIris numeric normalized by range stats:\n")
+    # compute(iris_norm_range)
+    # print("\nIris numeric normalized by standard deviation stats:\n")
+    # compute(iris_norm_z)
+
+    # print("Cars numeric stats:\n")
+    # compute(cars_numeric)
+    # print("\nCars numeric normalized by range stats:\n")
+    # compute(cars_norm_range)
+    # print("\nCars numeric normalized by standard deviation stats:\n")
+    # compute(cars_norm_z)
+
+    iris_norm_range = pd.concat([iris_norm_range, iris['Species']], axis = 1)
+    cars_norm_range = pd.concat([cars_norm_range, cars['Type']], axis = 1)
+
+    # # Visualize data.
+    # visualize(data=iris, vars=iris_numeric.columns, title='Iris', hue=iris.columns[-1])
+    # visualize(data=iris_norm_range, vars=iris_numeric.columns, title='Iris', hue=iris.columns[-1])
+    # visualize(data=cars, vars=cars_numeric.columns, title='Cars', hue=cars.columns[2])
+    # visualize(data=cars_norm_range, vars=cars_numeric.columns, title='Cars', hue=cars.columns[2])
 
 if __name__=="__main__":
     main()
